16ca8b3e2621233ade10b649be1bbc10751532c2
Harry Cutts <hcutts@google.com>
2025-06-12 13:26:28 +0000

    Revert "TouchInputMapper: remove PointerUsage code"
    This reverts commit 0917213c6933a701bf1eb6bcfc273db6d7ed31da.
    
    Reason for revert: it causes small changes in the input event stream for
    drawing tablets and absolute mice, which don't affect compatibility but
    cause failures when the cleaned-up TouchInputMapper is tested against
    the old CTS tests.
    
    Reverted changes: /q/submissionid:33669427-remove-touchpad-stack-flag
    
    Bug: 420320431
    Test: $ atest CtsInputTestCases
    Flag: com.android.input.flags.disable_touch_input_mapper_pointer_usage
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:0814e813f1125373e20233ba0e5259733bd50396)
    Merged-In: I5d3ec0b75da78e89a50cd1a0e9623b3b495f177a
    Change-Id: I5d3ec0b75da78e89a50cd1a0e9623b3b495f177a

--- services/inputflinger/include/InputReaderBase.h
+++ services/inputflinger/include/InputReaderBase.h
@@ -161,7 +161,7 @@ struct InputReaderConfiguration {
     // For mice, these are ignored and the values of mousePointerSpeed and
     // mousePointerAccelerationEnabled used instead.
     //
-    // TODO(b/281840344): remove, and check if other fields beginning with pointer… are still used.
+    // TODO(b/281840344): remove this.
     VelocityControlParameters pointerVelocityControlParameters;
 
     // Velocity control parameters for mouse wheel movements.
