7ffdebe917f363c72fd1b23bbf68877d132d3b6f
rmp22 <195054967+rmp22@users.noreply.github.com>
2026-03-17 13:28:36 +0800

    [enhance] optimizing dalvik heap configs for lower ram tiers
    6gb: utilization 0.5->0.6 with usap/madvise/pinner, 4gb: bumping growthlimit and maxfree with usap/madvise, 2gb/1gb: raising minfree from 512k to reduce micro-gc churn
    Change-Id: I4495ee8708a100db9e13c00120bb94735f50a744

--- build/phone-xhdpi-8192-dalvik-heap.mk
+++ build/phone-xhdpi-8192-dalvik-heap.mk
@@ -16,15 +16,14 @@
 
 # Provides overrides to configure the Dalvik heap for an 8GB phone
 
-PRODUCT_VENDOR_PROPERTIES += \
+PRODUCT_PRODUCT_PROPERTIES += \
     dalvik.vm.heapstartsize?=16m \
     dalvik.vm.heapgrowthlimit?=256m \
     dalvik.vm.heapsize?=512m \
-    dalvik.vm.heaptargetutilization?=0.6 \
-    dalvik.vm.heapminfree?=12m \
-    dalvik.vm.heapmaxfree?=48m \
-    dalvik.vm.usap_pool_enabled?=true \
-    dalvik.vm.usap_pool_size_max?=3 \
-    dalvik.vm.madvise.vdexfile.size?=157286400 \
-    dalvik.vm.madvise.odexfile.size?=157286400 \
-    persist.sys.pinner.quota_pct?=12
+    dalvik.vm.heaptargetutilization?=0.5 \
+    dalvik.vm.heapminfree?=8m \
+    dalvik.vm.heapmaxfree?=32m \
+    dalvik.vm.madvise.vdexfile.size?=31457280 \
+    dalvik.vm.madvise.odexfile.size?=31457280 \
+    dalvik.vm.madvise.artfile.size?=0 \
+    ro.lmk.medium?=700
